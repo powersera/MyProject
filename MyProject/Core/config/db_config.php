bb90dc656105fe09ede310f37b50cbf8279a8a6a
powersera <sergeybarladin00@gmail.com>
2017-12-05 10:56:55 +0100

    Add files via upload

---
+++ MyProject/Core/config/db_config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Serhii
+ * Date: 09.11.2017
+ * Time: 15:47
+ */
+
+return [
+    'host' => 'localhost',
+    'login' =>'root',
+    'password' => 'root',
+    'db_name' => 'guestbook'
+];
